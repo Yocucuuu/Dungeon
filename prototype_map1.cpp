4ebc39d41e6d049a3220c5d312b66ce09524d73a
Yocucuuu <44639176+Yocucuuu@users.noreply.github.com>
2018-11-14 15:37:20 +0700

    Update prototype_map1.cpp

--- prototype_map1.cpp
+++ prototype_map1.cpp
@@ -1,15 +1,61 @@
 #include <iostream>
-
+#include <fstream>
 using namespace std;
 
 const int FMAP_x = 11;
 const int FMAP_y = 11;
 char map_b[50][50];
+char map_k[FMAP_y][FMAP_x];
+//save file lebih dari satu
+//setiap save file berisi 1 orang saja
+
+void LOAD(/*code disini*/)
+{
+    ifstream data_load;
+    data_load.open("save.txt");
+    if (data_load.is_open())
+    {
+        //code disini
+        data_load.close();
+    }
+    else
+    {
+        //code disini
+    }
+}
+
+void SAVE()
+{
+    ofstream data_save;
+    data_save.open("save.txt");
+    if (data_save.is_open())
+    {
+        //code disini
+    }
+    else
+    {
+        //code disini
+    }
+}
+void MAIN_MENU()
+{
+    int pilihan;
+    do
+    {
+        cout << "1. New Game";
+        cout << "2. Load Game";
+        cin >> pilihan;
+
+    } while (pilihan != 2);
+}
 
 int main()
 {
+    MAIN_MENU();
+
+
     int i,j;
-    char map_k[FMAP_y][FMAP_x];
+
     for (i=0;i<FMAP_y;i++)
     {
         for (j=0;j<FMAP_x;j++)
@@ -25,8 +71,6 @@ int main()
             }
         }
     }
-
-
 
 
     for(i=0;i<FMAP_y;i++){

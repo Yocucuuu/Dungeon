d9b311189e479486c9acd0e9f242db21ec3e189c
Yocucuuu <44639176+Yocucuuu@users.noreply.github.com>
2018-11-14 15:14:25 +0700

    Update prototype_map1.cpp

--- prototype_map1.cpp
+++ prototype_map1.cpp
@@ -1,77 +1,41 @@
 #include <iostream>
-#include <string>
-#include <fstream>
-#include <conio.h>
-#include <windows.h>
 
 using namespace std;
 
-void Gerak(char gerak,int &posx1, int &posy1, int &posx2 , int &posy2)
-{
-    if(kbhit())
-    {
-        gerak = getch();
-        if (gerak=='w')
-        {
-            posy1-=1;
-            posy2-=1;
-            //gerak w
-        }
-        else if (gerak=='s')
-        {
-            posy1+=1;
-            posy2+=1;
-            //gerak s
-        }
-        else if (gerak=='a')
-        {
-            posx1-=1;
-            posx2-=1;
-            //gerak a
-        }
-        else if (gerak=='d')
-        {
-            posx1+=1;
-            posx2+=1;
-            //gerak d
-        }
-    }
-}
+const int FMAP_x = 11;
+const int FMAP_y = 11;
+char map_b[50][50];
 
-int main() {
-    int posx1=0,posx2=11;
-    int posy1=0,posy2=11;
-    char gerak; //inisialisasi char untuk gerak
-    string mapping[30][30]; //inisialisasi map besar
-    do
+int main()
+{
+    int i,j;
+    char map_k[FMAP_y][FMAP_x];
+    for (i=0;i<FMAP_y;i++)
     {
-    for (int i=0;i<30;i++)
-    {
-        for (int j=0;j<30;j++)
+        for (j=0;j<FMAP_x;j++)
         {
-            mapping[i][j] = " ";
-        }
-    }
-    Gerak(gerak,posx1,posy1,posx2,posy2);
-    for (int i=0;i<30;i++)
-    {
-        for (int j=0;j<30;j++)
-        {
-            if ((i==posy1||i==posy2)||(j==posx1||j==posx2))
+            if (i==0||i==FMAP_y-1||j==0||j==FMAP_x-1)
             {
-                mapping[i][j] = "%";
+                map_k[i][j]='*';
             }
-            cout << mapping[i][j];
+            else
+
+            {
+                map_k[i][j]=' ';
+            }
         }
-        cout << "\n";
     }
-    cout << gerak << endl;
-    cout <<"posisi x atas:"<< posx1 << endl;
-    cout <<"posisi x bawah:"<< posx2 << endl;
-    cout <<"posisi y atas:"<< posy1 << endl;
-    cout <<"posisi y bawah:"<< posy2 << endl;
-    //getch();
-    Sleep(100);
-    system("cls");
-    } while (true);
+
+
+
+
+    for(i=0;i<FMAP_y;i++){
+        for(j=0;j<FMAP_x;j++){
+            cout<<map_k[i][j];
+        }cout<<endl;
+    }
+
+
+
+    return 0;
 }
